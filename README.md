92d46516f447e4ac010bfd483685189221bfdaa5
tonyketcham <tony@karmalies.studio>
2022-04-01 23:41:24 -0500

    hello world

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nannou sketches n stuff
+
+This is me learning Rust + Nannou to hopefully be able to make good use of the pen plotter I've just recently received :0
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use nannou::{prelude::*};
+
+fn main() {
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .run();
+}
+
+struct Model {
+    bg_color: rgb::Srgb<u8>,
+    x: f32,
+    y: f32,
+    radius: f32,
+}
+
+fn model(_app: &App) -> Model {
+    Model {
+        bg_color: YELLOW,
+        x: 0.0,
+        y: 0.0,
+        radius: 100.0,
+    }
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    if model.radius < 500.0 {
+        model.radius += 1.0;
+    }
+}
+
+fn view(app: &App, model: &Model, frame: Frame){
+    let draw = app.draw();
+    draw.background()
+        .color(BLACK);
+    draw.ellipse()
+        .color(model.bg_color)
+        .w(model.radius)
+        .h(model.radius)
+        .x_y(model.x, model.y);
+    draw.to_frame(app, &frame).unwrap();
+}
